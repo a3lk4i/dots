826e86b6da129e0f580ce8f99d32930d127ce358
Akhil Kavuri <a3lk4i@gmail.com>
2023-05-11 14:56:10 +0530

    chore(nvchad): mappings

--- nvim/nvchad/custom/mappings.lua
+++ nvim/nvchad/custom/mappings.lua
@@ -1,5 +1,18 @@
 local M = {}
 
+M.general = {
+  n = {
+    [";"] = { ":", "enter command mode", opts = { nowait = true } },
+    ["<leader>sx"] = {
+      function()
+        require("telescope.builtin").resume()
+      end,
+      "resume telescope",
+      opts = { noremap = true, silent = true }
+    }
+  },
+}
+
 M.dap = {
   plugin = true,
   n = {
