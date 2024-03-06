85fb1c8c99f6b653d3e66f5f4c17dd9b53af7a45
Akhil Kavuri <a3lk4i@gmail.com>
2024-03-06 10:15:14 +0530

    chore: upgrade deps

--- lazyvim/nvim/lua/plugins/colorschemes.lua
+++ lazyvim/nvim/lua/plugins/colorschemes.lua
@@ -1,9 +1,10 @@
 return {
   { "rose-pine/neovim", name = "rose-pine" },
+  { "ellisonleao/gruvbox.nvim", priority = 1000, config = true, opts = ... },
   {
     "LazyVim/LazyVim",
     opts = {
-      colorscheme = "rose-pine",
+      colorscheme = "gruvbox",
     },
   },
 }
