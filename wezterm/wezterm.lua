f8871243314ca5404ededc11455a316239c09867
a3lk4i <a3lk4i@gmail.com>
2025-12-10 21:06:16 +0530

    chore(nu): clean up

--- wezterm/wezterm.lua
+++ wezterm/wezterm.lua
@@ -9,14 +9,15 @@ local config = wezterm.config_builder()
 -- ENV before shell
 local home_path = os.getenv("HOME") or "/home/a3l"
 config.set_environment_variables = {
-    XDG_CONFIG_HOME = home_path .. "/.config",
+  XDG_CONFIG_HOME = home_path .. "/.config",
 }
 
 local is_linux = function()
-    return wezterm.target_triple:find("linux") ~= nil
+  return wezterm.target_triple:find("linux") ~= nil
 end
 
-config.default_prog = is_linux() and { "/home/linuxbrew/.linuxbrew/bin/nu" } or { "/opt/homebrew/bin/nu" }
+config.default_prog = is_linux() and { "/home/linuxbrew/.linuxbrew/bin/nu" }
+  or { "/opt/homebrew/bin/nu" }
 
 -- UI stuff
 config.tab_bar_at_bottom = true
@@ -28,53 +29,53 @@ config.color_scheme_dirs = { home_path .. "/.config/wezterm/colors" }
 -- wezterm.gui is not available to the mux server, so take care to
 -- do something reasonable when this config is evaluated by the mux
 local function get_appearance()
-    if wezterm.gui then
-        return wezterm.gui.get_appearance()
-    end
-    return "Dark"
+  if wezterm.gui then
+    return wezterm.gui.get_appearance()
+  end
+  return "Dark"
 end
 
 local function scheme_for_appearance(appearance)
-    if appearance:find("Dark") then
-        return "melange_dark"
-    else
-        return "melange_light"
-    end
+  if appearance:find("Dark") then
+    return "melange_dark"
+  else
+    return "melange_light"
+  end
 end
 config.color_scheme = scheme_for_appearance(get_appearance())
 
-config.font_size = is_linux() and 12.5 or 15
+config.font_size = is_linux() and 13 or 16
 config.window_padding = {
-    left = 10,
-    right = 10,
-    top = 10,
-    bottom = 10,
+  left = 10,
+  right = 10,
+  top = 10,
+  bottom = 10,
 }
 
 -- Keybindings
 config.keys = {}
 for i = 1, 8 do
-    -- CTRL + number to activate that tab
-    table.insert(config.keys, {
-        key = tostring(i),
-        mods = "CTRL",
-        action = act.ActivateTab(i - 1),
-    })
-    -- F1 through F8 to activate that tab
-    table.insert(config.keys, {
-        key = "F" .. tostring(i),
-        action = act.ActivateTab(i - 1),
-    })
+  -- CTRL + number to activate that tab
+  table.insert(config.keys, {
+    key = tostring(i),
+    mods = "CTRL",
+    action = act.ActivateTab(i - 1),
+  })
+  -- F1 through F8 to activate that tab
+  table.insert(config.keys, {
+    key = "F" .. tostring(i),
+    action = act.ActivateTab(i - 1),
+  })
 end
 table.insert(config.keys, {
-    key = "t",
-    mods = "CTRL",
-    action = act.SpawnTab("CurrentPaneDomain"),
+  key = "t",
+  mods = "CTRL",
+  action = act.SpawnTab("CurrentPaneDomain"),
 })
 table.insert(config.keys, {
-    key = "w",
-    mods = "CTRL",
-    action = wezterm.action.CloseCurrentTab({ confirm = true }),
+  key = "w",
+  mods = "CTRL",
+  action = wezterm.action.CloseCurrentTab({ confirm = true }),
 })
 
 -- and finally, return the configuration to wezterm
